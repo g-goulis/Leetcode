ccaf2166f595827928ac2a096b316a8cea5db3f7
Gabriel Goulis <ninjagabe2003@gmail.com>
2023-08-09 23:36:49 -0500

    Container With Most Water

--- Neetcode/Two Pointers/167_Two_Sum_II/Solution.java
+++ Neetcode/Two Pointers/167_Two_Sum_II/Solution.java
@@ -1,35 +1,28 @@
-import java.util.HashMap;
-
-public class Solution {
+class Solution {
     public int[] twoSum(int[] numbers, int target) {
-        class Solution {
-            public int[] twoSum(int[] numbers, int target) {
-                int p1, p2;
-                int curr;
-                p1 = 0;
-                p2 = numbers.length - 1;
+        int p1, p2;
+        int curr;
+        p1 = 0;
+        p2 = numbers.length - 1;
 
-                while(p1 < numbers.length) {
-                    curr = numbers[p1] + numbers[p2];
-                    if(curr == target) {
-                        return new int[]{p1 + 1, p2 + 1};
-                    }
+        while(p1 < numbers.length) {
+            curr = numbers[p1] + numbers[p2];
+            if(curr == target) {
+                return new int[]{p1 + 1, p2 + 1};
+            }
 
-                    if(curr < target) {
-                        p1++;
-                    } else {
-                        p2--;
-                    }
-                }
-
-
-                return new int[]{};
+            if(curr < target) {
+                p1++;
+            } else {
+                p2--;
             }
         }
+
 
         return new int[]{};
     }
 }
+
 //public class Optimal {
 //
 //    public int[] twoSum(int[] nums, int target) {
